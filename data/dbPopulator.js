42f80cea7f5d12d4bbe997e3153718c6b43cf567
decrypto27 <vijay.kumar.attri@outlook.com>
2017-01-19 17:53:16 +0530

    bank detail populator made from json files

---
+++ data/dbPopulator.js
@@ -0,0 +1,63 @@
+var Promise     = require('bluebird');
+var request     = require('request');
+var bankInfo    = require('../urlmappings.json');
+var fs          = require('fs');
+var https       = require('https');
+var path        = require('path');
+var async       = require('async');
+var dbManager        = require('../utilities/dbManager');
+
+
+async.forEachSeries(Object.keys(bankInfo), function(item,callback){
+    var resultWrapper = {};
+    var filePath = path.resolve(__dirname, item.toString() +'.json')
+    var tasks = [
+        readWrapper.bind(null,filePath,resultWrapper),
+        batchInserter.bind(null, resultWrapper)
+    ];
+    async.series(tasks, function(error){
+        if(error){
+            return callback(error);
+        }
+        console.log('all insertions done')
+        return callback(null);
+    })
+});
+function readWrapper(filePath , resultWrapper, callback){
+    fs.readFile(filePath, function(err,data){
+        if(err){
+            return callback(err);
+        }
+        console.log(data);
+        resultWrapper.data = data;
+        return callback(null);
+    });
+}
+function batchInserter(wrapper, cb){
+    async.forEach(wrapper.data,function(item ,callback){
+        inserter(item).then(() => {
+            return callback(null);
+        }, (error) => {
+            return callback(error);
+        });
+    }, function(error){
+        return cb(error);
+    });
+}
+
+function inserter(data){
+
+    return new Promise( (resolve,reject) => {
+        var stmt = " INSERT INTO tb_bank_details SET ?" ;
+
+        var queryObj = {
+            stmt: stmt,
+            args: [data]
+        };
+        dbManager.runQuery(queryObj).then((result) => {
+            resolve(result);
+        }, (error) => {
+            reject(error);
+        });
+    });
+}
